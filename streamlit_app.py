6220450cd4daa6439beca4b8013311755667674f
kirangaikwad-eaton <KiranAGaikwad@eaton.com>
2025-12-02 19:52:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,8 @@ if ingredients_list:
     
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data = smoothiefroot_response.jason(), use_container_width= True)
+st.text(smoothiefroot_response.jason())
+#sf_df = st.dataframe(data = smoothiefroot_response.jason(), use_container_width= True)
     
 
 
