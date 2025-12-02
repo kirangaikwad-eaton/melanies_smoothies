13c92492a74c5f612f8bb93c6b1fe9bcdde92a29
kirangaikwad-eaton <KiranAGaikwad@eaton.com>
2025-12-02 20:37:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(
     col('FRUIT_NAME'),
-    col('SEARCH_ON')
+    col('SEARCH_ON'))
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
